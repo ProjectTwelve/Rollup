23369ae170fd1c1b5ae0d742cca9954ca301ece6
River <754865974@qq.com>
2022-12-12 16:52:58 +0800

    🎈 perf(contracts): gas optimization

--- contracts/RollUpStorage.sol
+++ contracts/RollUpStorage.sol
@@ -2,28 +2,16 @@
 pragma solidity ^0.8.17;
 
 contract RollUpStorage {
-    // side chainId
-    uint256 internal _chainId;
+ 
+  // verified transaction
+  mapping(bytes32 => address) internal _verified;
+  struct Tx {
+    bytes32 rlpTxHash;
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+  }
 
-    // verified transaction
-    mapping(bytes32 => bool) internal _isVerified;
-
-    struct Tx {
-        address from;
-        uint256 txType;
-        uint256 chainId;
-        uint256 nonce;
-        uint256 maxPriorityFeePerGas;
-        uint256 maxFeePerGas;
-        uint256 gasLimit;
-        address to;
-        uint256 value;
-        bytes data;
-        bytes[] accessList; // The above is the original transaction information
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-    }
-
-    uint256[48] private __gap;
+  uint256[49] private __gap;
 }
+
