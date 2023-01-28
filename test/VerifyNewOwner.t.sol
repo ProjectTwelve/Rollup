eddf70e1efbe91d63386598c4f90238447448306
Noy <noy_gpg.2fcap@simplelogin.com>
2023-01-28 17:56:15 +0800

    🐳 chore: owner validate

---
+++ test/VerifyNewOwner.t.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.17;
+
+import 'forge-std/Test.sol';
+import 'forge-std/Vm.sol';
+import 'forge-std/console.sol';
+
+import { RollUpgradable } from 'contracts/RollUpgradable.sol';
+
+import { ERC1967Proxy } from '@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol';
+
+import { UUPSUpgradeable } from '@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol';
+
+contract VerifyNewOwner is Test {
+  uint256 bnbChainFork;
+
+  RollUpgradable private _newImplementation;
+  UUPSUpgradeable private _proxy;
+
+  function setUp() public {
+    string memory bnbChainRpcUrl = vm.envString('BNB_CHAIN_URL');
+
+    bnbChainFork = vm.createFork(bnbChainRpcUrl);
+    vm.selectFork(bnbChainFork);
+    // select a specific number
+    vm.rollFork(25174100);
+
+    _newImplementation = new RollUpgradable();
+
+    _proxy = UUPSUpgradeable(0x80697eE36B4cc09a7F1dDFC30B2C6447Cc2F2785);
+  }
+
+  function testOwnerNotChangeAfterUpgrade() public {
+    address oldOwner = RollUpgradable(address(_proxy)).owner();
+    vm.prank(oldOwner);
+    _proxy.upgradeTo(address(_newImplementation));
+
+    address newOwner = RollUpgradable(address(_proxy)).owner();
+
+    assertEq(oldOwner, newOwner);
+  }
+}
